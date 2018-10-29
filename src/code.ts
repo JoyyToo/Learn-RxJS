e4b48e4600383f0258aaf289394b7e1b5c3e670d
joyytoo <joytoo97@gmail.com>
2018-10-29 14:47:35 +0300

    Create observable and subscribe to it

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,32 @@
-import {Observable as Rx} from "rxjs";
+import { Observable } from  "rxjs";
 
-console.log(Rx);
+var observable = Observable.create((observer:any) => {
+    try {
+    observer.next('Hey guys')
+    observer.next('How are you?')
+    setInterval(() => {
+        observer.next('I am good')
+    }, 2000)
+    // observer.complete()
+    // observer.next('Wont send')
+    } catch(err) {
+        observer.error(err)
+    }
+});
+
+var observer = observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    () => addItem('Completed')
+);
+
+setTimeout(() => {
+    observer.unsubscribe()
+}, 6000)
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
